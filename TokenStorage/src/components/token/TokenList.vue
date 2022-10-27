692a240bcdecc0ad005185b76593defec8e96c1c
EvgenySychev <samara808@yandex.ru>
2022-10-27 17:08:20 +0400

    added items in TokenList.vue

---
+++ TokenStorage/src/components/token/TokenList.vue
@@ -0,0 +1,69 @@
+<template>
+  <div>
+    <br>
+    <b-row>
+      <b-col md="12">
+        <div class="table-responsive">
+          <table class="table table-striped">
+            <thead>
+            <tr>
+              <th>Domain name</th>
+              <th>Token</th>
+              <th>Action</th>
+            </tr>
+            </thead>
+            <tbody>
+            <tr
+              v-for="token in tokens"
+              :key="token.id">
+              <td>{{ token.domain }}</td>
+              <td>{{ token.value }}</td>
+              <td>
+                <button>Delete</button>
+              </td>
+            </tr>
+            </tbody>
+          </table>
+        </div>
+      </b-col>
+    </b-row>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'TokenList',
+  data () {
+    return {
+      tokens: [
+        {
+          id: '1',
+          domain: 'https://tra-ta-ta1.ru',
+          value: '1234-4567-8901'
+        },
+        {
+          id: '2',
+          domain: 'https://tra-ta-ta2.ru',
+          value: '1234-4567-8902'
+        },
+        {
+          id: '3',
+          domain: 'https://tra-ta-ta3.ru',
+          value: '1234-4567-8903'
+        },
+        {
+          id: '4',
+          domain: 'https://tra-ta-ta4.ru',
+          value: '1234-4567-8904'
+        }
+      ]
+    }
+  },
+  created () {
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
--- TokenStorage/src/router/index.js
+++ TokenStorage/src/router/index.js
@@ -1,15 +1,28 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+import Home from '../components/Home'
+import NotFound from '../components/error-pages/NotFound'
+import TokenList from '../components/token/TokenList'
 
 Vue.use(Router)
 
 export default new Router({
+  mode: 'history',
   routes: [
     {
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      name: 'Home',
+      component: Home
+    },
+    {
+      path: '/token/list',
+      name: 'TokenList',
+      component: TokenList
+    },
+    {
+      path: '*',
+      name: 'NotFound',
+      component: NotFound
     }
   ]
 })
